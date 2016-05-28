8dd6fbcf16ceacdfdf24f6fd9468076b0e1881e5
Elmar Hinz <t3elmar@gmail.com>
2016-05-28 17:15:33 +0200

    Creating ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Double speed TypoScript Parser',
+	'description' => 'Classes to parse TypoScript with double speed.',
+	'category' => 'fe',
+	'author' => 'Elmar Hinz',
+	'author_email' => 't3elmar@gmail.com',
+	'state' => 'alpha',
+	'uploadfolder' => false,
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '8.0.0-8.99.99',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+	'createDirs' => NULL,
+);
+
